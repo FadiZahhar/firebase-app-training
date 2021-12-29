6c187a29ad30b644c789c18ed293077c3f2e2cba
Fadi Zahhar <79freelancer@gmail.com>
2021-12-29 20:32:27 +0200

    adding the firebase host test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import firebase from './FirebaseConfig';
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="title-row">
+        <h1 className="title">Firebase Recipies</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
